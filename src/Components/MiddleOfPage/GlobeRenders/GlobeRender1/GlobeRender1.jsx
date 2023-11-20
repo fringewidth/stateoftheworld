ecde841102350a6da296ad116c0aa142d20b7998
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2023-11-20 22:03:25 +0530

    Remove draggable globe

--- src/Components/MiddleOfPage/GlobeRenders/GlobeRender1/GlobeRender1.jsx
+++ src/Components/MiddleOfPage/GlobeRenders/GlobeRender1/GlobeRender1.jsx
@@ -1,12 +1,11 @@
 import "./GlobeRender1.css";
 import Globe from "../../../Globe/Globe";
-import { motion } from "framer-motion";
 
 function GlobeRender1() {
   return (
-    <motion.div drag>
+    <div>
       <Globe />
-    </motion.div>
+    </div>
   );
 }
 
