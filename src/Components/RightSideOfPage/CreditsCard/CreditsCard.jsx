78c89a1c945be85e9f026cb2a46f0a946026897d
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-02-27 11:46:29 +0530

    Update CreditsCard.jsx

--- src/Components/RightSideOfPage/CreditsCard/CreditsCard.jsx
+++ src/Components/RightSideOfPage/CreditsCard/CreditsCard.jsx
@@ -3,7 +3,10 @@ import "./CreditsCard.css";
 function CreditsCard() {
   return (
     <div>
-      <h1>CreditsCard</h1>
+      <h1>Hrishik Sai</h1>
+      <h1>Dharmisht SVK</h1>
+      <h1>Dhyaan Kotian</h1>
+      <h1>J Krishna Kaarthik</h1>
     </div>
   );
 }
