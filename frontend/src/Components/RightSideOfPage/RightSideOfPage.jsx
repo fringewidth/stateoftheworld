3ae9095dcaf90fcbcacf2ac1f24fb670447415dd
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-05-26 17:41:50 +0530

    Refactor RightSideOfPage component

--- frontend/src/Components/RightSideOfPage/RightSideOfPage.jsx
+++ frontend/src/Components/RightSideOfPage/RightSideOfPage.jsx
@@ -8,8 +8,14 @@ function RightSideOfPage(props) {
   return (
     <>
       <div className="relative text-white order-3 p-2 max-md:order-2 w-1/5 max-md:w-[100lvw] max-md:h-[100lvh] max-md:mt-20 max-md:mb-20 flex items-center justify-between flex-col">
-        <Sentiment countryData={props.countryData} />
-        <InfoCard countryData={props.countryData} country={props.country} />
+        <Sentiment
+        // countryData={props.countryData}
+        />
+        <InfoCard
+          newCountryData={props.newCountryData}
+          // country={props.country}
+        />
+        {/* Make this responsive */}
         <CreditsCard />
       </div>
     </>
