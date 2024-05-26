d1ff31339ac21ee0223f45e1755adc0e4af83a73
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-05-26 14:46:00 +0530

    Add a 'global' country in the API logic

--- backend/routes/monthsRoute.js
+++ backend/routes/monthsRoute.js
@@ -9,6 +9,30 @@ router.get("/:month/:year", async (req, res) => {
   const year = Number(req.params.year);
   try {
     const monthData = await months.find({ month: month, year: year });
+    let global = {
+      code: "global",
+      country: "Global",
+      tempAnomaly: 0,
+      coconc: 0,
+      no2conc: 0,
+      o3conc: 0,
+      so2conc: 0,
+      news: [],
+    };
+
+    let length = monthData[0].countries.length;
+
+    monthData[0].countries.forEach((country) => {
+      global.tempAnomaly += country.tempAnomaly / length;
+      global.coconc += country.coconc / length;
+      global.no2conc += country.no2conc / length;
+      global.o3conc += country.o3conc / length;
+      global.so2conc += country.so2conc / length;
+      global.news = global.news.concat(country.news);
+    });
+
+    monthData[0].countries.push(global);
+
     res.json(monthData);
   } catch (err) {
     res.status(500).json({ message: err.message });
