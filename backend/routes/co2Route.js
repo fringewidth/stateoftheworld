b343833d11f9a45fcc3e3fca6bf338277596afa0
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-06-04 23:52:06 +0530

    Modify CO2 API logic to return all countries at once

--- backend/routes/co2Route.js
+++ backend/routes/co2Route.js
@@ -4,17 +4,15 @@ const router = express.Router();
 
 const countryData = require("../azure/historical/countries.json");
 
-router.get("/:countryCode", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    let countryco2;
-    if (req.params.countryCode === "global") {
-      countryco2 = 4.07;
-    } else {
-      countryco2 = countryData.find(
-        (country) => country.code === req.params.countryCode
-      )["2020co2"];
-    }
-    res.json({ co2TonsPerCapita: countryco2 });
+    const response = countryData.reduce((acc, curr) => {
+      acc[curr.code] = curr["2020co2"];
+      return acc;
+    }, {});
+    response.global = 4.465;
+
+    res.json(response);
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.log(err);
